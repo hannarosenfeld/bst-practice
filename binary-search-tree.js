2a64d2e4d7b1091e2b624b247563c8615cd9d76f
carlstone7789 <carlstone7789@gmail.com>
2023-03-01 17:29:10 -0500

    lets try this again

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,4 +1,133 @@
 // Before starting, copy and paste your guided practice work from
 // `binary-search-tree.js` into this file
+// Do not change this
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
 
+class BinarySearchTree {
+
+  constructor() {
+    this.root = null;
+  }
+
+/////////////////////////////////
+
+insert(val, currentNode=this.root) {
+  // Your code here
+  const newNode = new TreeNode(val);
+  // if there is no root
+  if (!this.root) {
+    this.root = newNode;
+    return this;
+  }
+  // if the val is less than the currentNode's value
+  // go to the left
+  if (val < currentNode.val) {
+    // check if there is anything to the left
+    // if not that's where the new node goes
+    if (!currentNode.left) {
+      currentNode.left = newNode;
+    } else {
+      // otherwise recurse to the left
+      return this.insert(val, currentNode.left);
+    }
+  }
+
+  // if the val is greater than the currentNode's value
+  // go to the right
+  if (val > currentNode.val) {
+    // check if there is anything to the right
+    if (!currentNode.right) {
+      // if not that's where the node goes
+      currentNode.right = newNode;
+    }else {
+      // otherwise recurse to the right
+      return this.insert(val, currentNode.right);
+    }
+
+  }
+
+}
+/////////////////////////////////
+  /////////////////////////////
+
+  search(val) {
+    let currentNode = this.root;
+
+    while(currentNode)
+    {
+      if (currentNode.val === val) return true;
+
+      if (currentNode.val > val) {
+        currentNode = currentNode.left;
+      }
+      else currentNode = currentNode.right;
+    }
+    return false
+  }
+
+
+  preOrderTraversal(currentNode = this.root) {
+    if (!currentNode) return this;
+    console.log(currentNode.val)
+    this.preOrderTraversal(currentNode.left)
+    this.preOrderTraversal(currentNode.right)
+  }
+
+
+  inOrderTraversal(currentNode = this.root) {
+    if (!currentNode) return this;
+    this.inOrderTraversal(currentNode.left)
+    console.log(currentNode.val)
+    this.inOrderTraversal(currentNode.right)
+
+  }
+
+
+  postOrderTraversal(currentNode = this.root) {
+    if (!currentNode) return this;
+    this.postOrderTraversal(currentNode.left)
+    this.postOrderTraversal(currentNode.right)
+    console.log(currentNode.val)
+
+
+  }
+
+    // Breadth First Traversal - Iterative
+  breadthFirstTraversal() {
+    if (!this.root) return this;
+    const queue = []
+    queue.push(this.root)
+
+    while(queue.length) {
+      const curr = queue.shift();
+      console.log(curr.val);
+      if (curr.left) queue.push(curr.left);
+      if (curr.right) queue.push(curr.right);
+    }
+    return this
+  }
+
+  // Depth First Traversal - Iterative
+  depthFirstTraversal() {
+    if (!this.root) return this;
+    const stack = []
+    stack.push(this.root)
+
+    while(stack.length) {
+      const curr = stack.pop();
+      console.log(curr.val);
+      if (curr.left) stack.push(curr.left);
+      if (curr.right) stack.push(curr.right);
+    }
+    return this
+  }
+
+}
 // Your code here
+module.exports = { TreeNode, BinarySearchTree }
--- tree-practice.js
+++ tree-practice.js
@@ -4,29 +4,106 @@ const { BinarySearchTree, TreeNode } = require('./binary-search-tree.js');
 
 // Practice problems on binary trees
 
-function findMinBST (rootNode) {
+function findMinBST (rootNode = this.root) {
   // Your code here
+    if (rootNode.left == null)
+        return rootNode.val;
+
+    return findMinBST(rootNode.left);
 }
+
 
 function findMaxBST (rootNode) {
   // Your code here
+  if (rootNode.right == null)
+  return rootNode.val;
+return findMaxBST(rootNode.right)
 }
+
+///////////////////////////////
+
 
 function findMinBT (rootNode) {
-  // Your code here
+    if (rootNode === null) return;
+
+    let res = rootNode.val;
+    let lres = findMinBT(rootNode.left);
+    let rres = findMinBT(rootNode.right);
+
+    if (lres < res) res = lres;
+    if (rres < res) res = rres;
+    return res;
 }
+
+////////////////////////////////
 
 function findMaxBT (rootNode) {
-  // Your code here
+  if (rootNode === null) return;
+
+  let res = rootNode.val;
+  let lres = findMaxBT(rootNode.left);
+  let rres = findMaxBT(rootNode.right);
+
+  if (lres > res) res = lres;
+  if (rres > res) res = rres;
+  return res;
 }
+// Roger's Solution
+// function findMaxBT (rootNode) {
+//   //!!START
+//   let min = rootNode.val;
+
+//   if (rootNode.left) min = Math.max(min, findMaxBT(rootNode.left));
+//   if (rootNode.right) min = Math.max(min, findMaxBT(rootNode.right));
+
+//   return min;
+//   //!!END
+// }
 
 function getHeight (rootNode) {
-  // Your code here
+  //if no node, return
+  if (!rootNode) return -1;
+  if (!rootNode.left && !rootNode.right) return 0;
+  else {
+    let ldepth = getHeight(rootNode.left)
+    let rdepth = getHeight(rootNode.right)
+
+    if (ldepth > rdepth) return (ldepth + 1);
+    else return (rdepth+1)
+  }
 }
 
+// function getHeight (rootNode) {
+//   //!!START
+//   if (!rootNode) return -1;
+//   if (!rootNode.left && !rootNode.right) return 0;
+//   return 1 + Math.max(getHeight(rootNode.left), getHeight(rootNode.right));
+//   //!!END
+// }
+
 function balancedTree (rootNode) {
-  // Your code here
+  // if (!rootNode) return false;
+
+
+  // let ldepth = getHeight(rootNode.left);
+  // let rdepth = getHeight(rootNode.right);
+  // //console.log(`IM RIGHT HERE!!`,ldepth, rdepth)
+
+  // if (ldepth === rdepth) return true;
+  // else if (ldepth +1 === rdepth || ldepth -1 === rdepth) return true;
+  // return false
+  let queue = [rootNode];
+  while (queue.length) {
+    let curr = queue.shift();
+
+    if (Math.abs(getHeight(curr.left) - getHeight(curr.right)) <= 1) {
+      if (curr.left) queue.push(curr.left)
+      if (curr.right) queue.push(curr.right)
+    } else return false
+  }
+  return true
 }
+
 
 function countNodes (rootNode) {
   // Your code here
@@ -55,7 +132,7 @@ function deleteNodeBST(rootNode, target) {
 
   // Case 2: Two children:
   //  Set the value to its in-order predecessor, then delete the predecessor
-  //  Replace target node with the left most child on its right side, 
+  //  Replace target node with the left most child on its right side,
   //  or the right most child on its left side.
   //  Then delete the child that it was replaced with.
 
